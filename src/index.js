fcd06a6f8f13590f9fb07d99245edd0193fdf3a0
Anders Kensby <anders.kensby@gmail.com>
2019-12-16 21:23:44 +0100

    initial commit

---
+++ src/index.js
@@ -0,0 +1,58 @@
+import cors from 'cors';
+import express from 'express';
+import jwt from 'jsonwebtoken';
+import mongoose from 'mongoose';
+import { ApolloServer, AuthenticationError } from 'apollo-server-express';
+
+import schemas from './schemas';
+import resolvers from './resolvers';
+
+import userModel from './models/userModel';
+import postModel from './models/postModel';
+
+const app = express();
+app.use(cors());
+
+const getUser = async (req) => {
+  const token = req.headers['token'];
+
+  if (token) {
+    try {
+      return await jwt.verify(token, 'riddlemethis');
+    } catch (e) {
+      throw new AuthenticationError('Your session expired. Sign in again.');
+    }
+  }
+};
+
+const server = new ApolloServer({
+  typeDefs: schemas,
+  resolvers,
+  context: async ({ req }) => {
+    if (req) {
+      const me = await getUser(req);
+
+      return {
+        me,
+        models: {
+          userModel,
+          postModel,
+        },
+      };
+    }
+  },
+});
+
+server.applyMiddleware({ app, path: '/graphql' });
+
+// docker run -d -p 27017:27017 -v ~/data:/data/db mongo
+
+app.listen(5000, () => {
+    mongoose.connect(
+        'mongodb://localhost:27017/graphql',
+        {
+            useCreateIndex: true,
+            useUnifiedTopology: true,
+            useNewUrlParser: true
+        });
+});
